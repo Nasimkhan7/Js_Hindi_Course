31dfaf0fb5ddd28759daaeab21b5a15d0893dfec
Nasim khan <91681768+Nasimkhan7@users.noreply.github.com>
2023-08-19 07:41:02 +0000

    conversion datatype

---
+++ 01_basics/03_conversion_Opration.js
@@ -0,0 +1,36 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+let scope = "12avd"  
+let valuesNumber =Number(scope)
+console.log(valuesNumber);  // NaN outPut
+
+// "33" string to convert in number => 33
+// "12avd" => output NaN not a Number
+
+// boolean convert  true => 1 ;false => 0;
+ //=========conversion ==============
+let isLoggedIn  =1;
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+// ""=> false
+// "Nasimkhan " => true
+
+ //=========conversion last ==============
+
+// string conversion====
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+  
