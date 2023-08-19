d76929f871330168dbdf190e9412c37646e6defd
Nasim khan <91681768+Nasimkhan7@users.noreply.github.com>
2023-08-19 06:12:05 +0000

    nn

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144254
+let accountEmail = "nasimkhan75709@gmail.com"
+var accountPassword= "129101"
+accountCity = "naranapur"
+
+// accountId=2         //not allowed changes variable
+
+accountEmail= "khan@"
+accountPassword="111112"
+accountCity="patherdewa"
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])
